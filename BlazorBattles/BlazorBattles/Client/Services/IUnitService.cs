6de170d934d3cee01a496d9af3cf8674b5c7ea10
Marios-Kyriakos Kontzos <Marios-Kyriakos.Kontzos@gr.ey.com>
2021-10-19 19:30:07 +0300

    Section 3: Completed

---
+++ BlazorBattles/BlazorBattles/Client/Services/IUnitService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorBattles.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+   public interface IUnitService
+   {
+      public IList<Unit> Units { get;}
+      public IList<UserUnit> MyUnits { get; set; }
+      public void AddUnit(int unitId);
+   }
+}
