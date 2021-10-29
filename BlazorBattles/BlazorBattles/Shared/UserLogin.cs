7dcaf650edd1eb70a7e093b0f3e4d87e107aed9e
Marios-Kyriakos Kontzos <Marios-Kyriakos.Kontzos@gr.ey.com>
2021-10-29 19:40:38 +0300

    Completed Section 4

---
+++ BlazorBattles/BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+   public class UserLogin
+   {
+      [Required(ErrorMessage = "Please enter a user name.")]
+      public string UseName { get; set; }
+      [Required(ErrorMessage = "Please enter a password.")]
+      public string Password { get; set; }
+   }
+}
